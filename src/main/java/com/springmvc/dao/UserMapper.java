6b0b967bf4140a959b75c5dc654fd691e7d219f2
jinxscorpio <jinkexyz@126.com>
2017-10-17 23:23:32 +0800

    Add Users

---
+++ src/main/java/com/springmvc/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package com.springmvc.dao;
+
+import com.springmvc.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+}
